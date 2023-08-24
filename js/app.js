64f4c91b1420fcced88ac52468c083e738306aab
onhope <chch4934@gmail.com>
2023-08-24 16:54:13 +0900

    start 버튼 클릭 이벤트 추가

--- js/app.js
+++ js/app.js
@@ -1,12 +1,29 @@
 const container = document.querySelector(".image-container");
 const StartButton = document.querySelector(".start-buton");
+const playTime = document.querySelector(".play-time");
 const gameText = document.querySelector(".game-text");
+
+let isPlaying = false;
+let time = 0;
+let timeInterval;
+
 
 const dragged = {
   el : null,
   class : null,
   index: null
 } 
+
+StartButton.addEventListener("click", () => {
+  setGame();
+})
+
+function setGame() {
+  timeInterval = setInterval( () => {
+    time++;
+    playTime.innerText = time;
+  }, 1000)
+}
 
 container.addEventListener("dragstart", e => {
   const obj = e.target; // 드래그 한 위치의 target 속성
@@ -28,7 +45,7 @@ container.addEventListener("drop", e => {
     originPlace = dragged.el.previousSibling;
     isLast = True;
   }
-  const droppedIndex = ([...obj.parentNode.children].indexOf(obj)); // 드롭된 위피의 인덱스를 반환
+  const droppedIndex = ([...obj.parentNode.children].indexOf(obj)); // 드롭된 위치의 인덱스를 반환
   // console.log(droppedIndex);
   dragged.index > droppedIndex ? obj.before(dragged.el) : obj.after(dragged.el);  
   isLast ? originPlace.after(obj) : originPlace.before(obj);
